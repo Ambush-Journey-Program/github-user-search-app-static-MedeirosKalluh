6265f9e81a6e8cfa5be13d1a59244c512c6fff60
Lucas Medeiros <lucaspontoe@hotmail.com>
2023-01-30 18:57:07 -0300

    Implement variables for GIT USERs API Create Function responsable for fetch the API Add the data to the submit event

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 //dark mode
 function changeMode() {
     const element = document.body;
-    element.classList.toggle("dark-mode");}
+    element.classList.toggle("dark-mode");
+}
 
-
-// Initial state
+// Set API
+const apiGit = `https://api.github.com`  
+const searchInput = document.querySelector(".search__bar")
+async function fetchResponse(url) {
+    const response = await fetch(url);
+    const info = await response.json();
+    return info
+} 
 
 const cardEl = document.querySelector(".card")
 const initialState = document.querySelector(".card__initial")
 const searchEl = document.querySelector(".search")
-searchEl.addEventListener('submit', function(event){
+searchEl.addEventListener('submit', async function(event){
     event.preventDefault();
     cardEl.style.display = "grid";
-    initialState.style.display = "none";  
+    initialState.style.display = "none";
+    const apiSource = `${apiGit}/users/${searchInput.value}`
+    const infoApi = await fetchResponse(apiSource)
+    console.log(infoApi)
 })
