3be4b903af63b9c9b305d76de198c8dbdbcbcf62
Lucas Medeiros <lucaspontoe@hotmail.com>
2023-01-31 18:57:34 -0300

    Fix nomenclature Adjust Variables position Make getUser function more eficient create Ids in the DOM for further needs

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
-//dark mode
+//Dark mode
 function changeMode() {
     const element = document.body;
     element.classList.toggle("dark-mode");
 }
 
 // Set API
-const apiGit = `https://api.github.com`  
+
+const cardEl = document.querySelector(".card")
+const initialState = document.querySelector(".card__initial")
+const loading = document.querySelector(".loading")
+const searchEl = document.querySelector(".search")
 const searchInput = document.querySelector(".search__bar")
-async function getUser(url) {
-    const response = await fetch(url);
+const API_GIT  = `https://api.github.com`
+
+searchEl.addEventListener('submit', async function(event){
+    event.preventDefault();
+    setLoadingScreen();
+    const infoApi = await getUser(searchInput.value)
+    // writeHtmlCardInfo(infoApi)
+    setCardScreen()
+})
+
+async function getUser(username) {
+    const response = await fetch(`${API_GIT}/users/${username}`);
     const info = await response.json();
     return info
 }
 
-const cardEl = document.querySelector(".card")
-const initialState = document.querySelector(".card__initial")
-const searchEl = document.querySelector(".search")
-const loading = document.querySelector(".loading")
-searchEl.addEventListener('submit', async function(event){
-    event.preventDefault();
-    setLoadingScreen();
-    const apiSource = `${apiGit}/users/${searchInput.value}`
-    const infoApi = await getUser(apiSource)
-    writeHtmlCardInfo(infoApi)
-    setCardScreen()
-})
 
 function setLoadingScreen() {
     initialState.style.display = "none";
+    cardEl.style.display = "none"
     loading.style.display = "flex";
 }
 
 function setCardScreen(){
     loading.style.display = "none";
     cardEl.style.display = "grid";
-}
-
-function writeHtmlCardInfo(infoApi){
-    console.log(infoApi)
 }
