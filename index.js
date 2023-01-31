43fd9552b380052e0d3ff0a743cd54d98b21e77a
Lucas Medeiros <lucaspontoe@hotmail.com>
2023-01-31 11:06:37 -0300

    organize code with functions

--- index.js
+++ index.js
@@ -7,22 +7,35 @@ function changeMode() {
 // Set API
 const apiGit = `https://api.github.com`  
 const searchInput = document.querySelector(".search__bar")
-async function fetchResponse(url) {
+async function getUser(url) {
     const response = await fetch(url);
     const info = await response.json();
     return info
-} 
+}
+
 const cardEl = document.querySelector(".card")
 const initialState = document.querySelector(".card__initial")
 const searchEl = document.querySelector(".search")
 const loading = document.querySelector(".loading")
 searchEl.addEventListener('submit', async function(event){
     event.preventDefault();
+    setLoadingScreen();
     const apiSource = `${apiGit}/users/${searchInput.value}`
+    const infoApi = await getUser(apiSource)
+    writeHtmlCardInfo(infoApi)
+    setCardScreen()
+})
+
+function setLoadingScreen() {
     initialState.style.display = "none";
     loading.style.display = "flex";
-    const infoApi = await fetchResponse(apiSource)
-    loading.style.display = "none";  
+}
+
+function setCardScreen(){
+    loading.style.display = "none";
     cardEl.style.display = "grid";
+}
+
+function writeHtmlCardInfo(infoApi){
     console.log(infoApi)
-})
+}
