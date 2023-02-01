7e7df0c4b803cf1e388c29b4875aa96dd24fcdc2
Lucas Medeiros <116045026+MedeirosKalluh@users.noreply.github.com>
2023-02-01 09:35:54 -0300

    Implement variables for GIT USERs API (#8)
    * Implement variables for GIT USERs API
    Create Function responsable for fetch the API
    Add the data to the submit event
    
    * Implemente loading screen
    
    * organize code with functions
    
    * Fix nomenclature
    Adjust Variables position
    Make getUser function more eficient
    create Ids in the DOM for further needs

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-//dark mode
+//Dark mode
 function changeMode() {
     const element = document.body;
-    element.classList.toggle("dark-mode");}
+    element.classList.toggle("dark-mode");
+}
 
-
-// Initial state
+// Set API
 
 const cardEl = document.querySelector(".card")
 const initialState = document.querySelector(".card__initial")
+const loading = document.querySelector(".loading")
 const searchEl = document.querySelector(".search")
-searchEl.addEventListener('submit', function(event){
+const searchInput = document.querySelector(".search__bar")
+const API_GIT  = `https://api.github.com`
+
+searchEl.addEventListener('submit', async function(event){
     event.preventDefault();
-    cardEl.style.display = "grid";
-    initialState.style.display = "none";  
+    setLoadingScreen();
+    const infoApi = await getUser(searchInput.value)
+    // writeHtmlCardInfo(infoApi)
+    setCardScreen()
 })
+
+async function getUser(username) {
+    const response = await fetch(`${API_GIT}/users/${username}`);
+    const info = await response.json();
+    return info
+}
+
+
+function setLoadingScreen() {
+    initialState.style.display = "none";
+    cardEl.style.display = "none"
+    loading.style.display = "flex";
+}
+
+function setCardScreen(){
+    loading.style.display = "none";
+    cardEl.style.display = "grid";
+}
